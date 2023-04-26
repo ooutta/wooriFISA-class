49c2ccb81289fe608a8f5c1d2bbc6a70853281b1
Kim Suhyun <93786956+ooutta@users.noreply.github.com>
2023-04-26 17:30:45 +0900

    javascript study

---
+++ 3.JavaScript/2.data_types/data_types.js
@@ -0,0 +1,7 @@
+let year = 2023;
+console.log(typeof year);
+
+let myName = "수현";
+console.log("안녕하세요. 저는 " + myName + "입니다.");
+console.log(`안녕하세요. 저는 ${myName}입니다.`);
+console.log(`안녕하세요. 저는 ${5+5}입니다.`);
