5715192ca73e672dba3926f58573091e50b3593f
Kim Suhyun <93786956+ooutta@users.noreply.github.com>
2023-04-27 14:39:09 +0900

    JS function, array sstudy

--- 3.JavaScript/7.array_string/1.array/array_basic.js
+++ 3.JavaScript/7.array_string/1.array/array_basic.js
@@ -1,1 +1,28 @@
-// 직접 같이 타이핑하며 학습
+// 1
+const array = ['orange', 'cookie', 'orange-cookie'];
+
+console.log(array);
+console.log(typeof array); // 기본 타입을 제외한 모든 것은 객체
+
+// Array 타입인지 확인
+console.log(Array.isArray(array));
+console.log(array.length);
+console.log(array[0], array[2]);
+
+// 2
+const strArr = ['orange', 'kiwi', 'apple'];
+delete strArr[1]; //희소배열이 생성되어서 비추천
+
+// 희소배열은 요소의 개수가 변하지 않기 때문에 헷갈림
+console.log(strArr, strArr.length);
+// > ['orange', 비어 있음, 'apple] 3
+
+strArr.push('cookie');
+console.log(strArr);
+// > ['orange', 비어 있음, 'apple', 'cookie']
+
+// 3
+// Array.(prototype).splice(삭제를 시작할 인덱스, 삭제할 요소)를 사용하여 요소 제거
+const nutArr = ['walnut', 'almond', 'pinenut'];
+nutArr.splice(1,1);
+console.log(nutArr);
